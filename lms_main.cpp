c0d97f09531d9c467b8a18c7db1415e6a9d949e5
Adhithyan KG <159514247+adhithyanKg@users.noreply.github.com>
2025-01-03 23:36:46 -0500

    refactored code
    Separate displayItems function to maintain code readability and print message when special author DVD borrowed

--- lms_main.cpp
+++ lms_main.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <memory>
 
+#define SPECIAL_AUTHOR_CONDITION "QPR"
+
 using namespace std;
 
 class LibraryItem{
@@ -12,6 +14,9 @@ class LibraryItem{
         string title;
         string author;
         int itemId;
+        bool isborrowed() const {
+            return borrowed;
+        }
     private:
         bool borrowed;
     public:
@@ -22,10 +27,10 @@ class LibraryItem{
         void returnItem(){
             borrowed=false;
         }
-        string isAvailable(){
+        string isAvailable() const {
             return borrowed ? "No" : "Yes";
         }
-        virtual void displayDetails()
+        virtual void displayDetails() const
         {
             cout << "Title: " << title << endl;
             cout << "Author: " << author << endl;
@@ -42,9 +47,9 @@ class Book : public LibraryItem{
         int pages;
     public:
         Book(string a, string b, int c, int d) : LibraryItem(a,b,c), pages(d){}
-        void displayDetails() override
+        void displayDetails() const override
         {
-            LibraryItem::displayDetails();
+           LibraryItem::displayDetails();
            cout << "Pages: " << pages << endl;
         }
         ~Book()
@@ -57,12 +62,15 @@ class DVD : public LibraryItem{
         int duration;
     public:
         DVD(string a, string b, int c, int d) : LibraryItem(a,b,c), duration(d){}
-        void displayDetails() override
+        void displayDetails() const override 
         {
             LibraryItem::displayDetails();
             cout << "Duration: " << duration << endl;
-            if(author == "QPR"){
-                cout << "This author has new DVD's, please checkout in section 1:21"<< endl;
+            if(isborrowed()){
+                cout << "Must be returned in 7 days"<< endl;
+                if(author == SPECIAL_AUTHOR_CONDITION){
+                    cout << "This author has new DVD's, please checkout in section 1:21"<< endl;
+                }
             }
         }
         ~DVD()
@@ -70,29 +78,31 @@ class DVD : public LibraryItem{
             cout << "DVD Destructor"<< endl;
         }
 };
-
-int main()
+void displayItems(const vector<unique_ptr<LibraryItem>>& items)
 {
-    vector<unique_ptr<LibraryItem>> items;
-    items.push_back(make_unique<Book>("ABC", "XYZ", 1, 92));
-    items.push_back(make_unique<DVD>("EFG", "QPR", 1, 45));
-    items.push_back(make_unique<DVD>("YRT", "KJH", 2, 68));
-    items.push_back(make_unique<Book>("KLM", "FRH", 2, 102));
-    
     for(auto& item: items)
     {
         item->displayDetails();
         cout << "----------------------------" << endl;
         
     }
+}
+int main()
+{
+    vector<unique_ptr<LibraryItem>> items;
+    
+    items.push_back(make_unique<Book>("ABC", "XYZ", 1, 92));
+    items.push_back(make_unique<DVD>("EFG", "QPR", 1, 45));
+    items.push_back(make_unique<DVD>("YRT", "KJH", 2, 68));
+    items.push_back(make_unique<Book>("KLM", "FRH", 2, 102));
+    
+    displayItems(items);
+    
     items[1]->borrowItem();
+    items[2]->borrowItem();
     cout << "After borrowed: " << endl;
     cout << "----------------------------" << endl;
     
-    for(auto& item: items)
-    {
-        item->displayDetails();
-        cout << "----------------------------" << endl;
-    }
+    displayItems(items);
     return 0;
 }
